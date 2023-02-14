6dd4bb8c2b582f00c9eec20e619d4b93211e509d
Annie <anniehartmann97@gmail.com>
2023-02-14 16:42:20 +0300

    Layout grid

--- layout/Layout.tsx
+++ layout/Layout.tsx
@@ -3,16 +3,16 @@ import { Header } from "./Header/Header";
 import { LayoutProps } from "./Layout.props";
 import { Sidebar } from "./Sidebar/Sidebar";
 import React, { FunctionComponent } from "react";
+import cn from "classnames";
+import styles from "./Layout.module.scss";
 
 const Layout = ({ children }: LayoutProps) => (
-  <>
-    <Header />
-    <div>
-      <Sidebar />
-      <div>{children}</div>
-    </div>
-    <Footer />
-  </>
+  <main className={styles.wrapper}>
+    <Header className={styles.header} />
+    <Sidebar className={styles.sidebar} />
+    <div className={styles.body}>{children}</div>
+    <Footer className={styles.footer} />
+  </main>
 );
 
 export const withLayout = <T extends Record<string, unknown>>(
