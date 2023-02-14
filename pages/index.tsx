2f7e253a23189b2f475c4abde591941498a5ded6
Annie <anniehartmann97@gmail.com>
2023-02-14 11:40:49 +0300

    P

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import { Button, Htag } from "../components";
+import { P } from "../components/P/P";
 
 export default function Home() {
   return (
@@ -14,6 +15,13 @@ export default function Home() {
       <Button appearance="ghost" onClick={() => console.log("ghost")}>
         Кнопка
       </Button>
+      <P size="m">
+        Студенты освоят не только hard skills, необходимые для работы
+        веб-дизайнером, но и soft skills — навыки, которые позволят эффективно
+        взаимодействовать в команде с менеджерами, разработчиками и
+        маркетологами. Выпускники факультета могут успешно конкурировать с
+        веб-дизайнерами уровня middle.
+      </P>
     </>
   );
 }
