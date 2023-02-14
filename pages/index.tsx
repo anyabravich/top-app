51612eb720299d08b6cc0cc5ebcaed25a197836b
Annie <anniehartmann97@gmail.com>
2023-02-14 18:45:15 +0300

    Add getStaticProps

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,11 @@
+import { GetStaticProps } from "next";
 import { useState } from "react";
 import { Button, Htag, P, Rating, Tag } from "../components";
 import { withLayout } from "../layout/Layout";
+import axios from "axios";
+import { MenuItem } from "../interfaces/menu.interface";
 
-function Home() {
+function Home({ menu }: HomeProps) {
   const [rating, setRating] = useState<number>(4);
   return (
     <>
@@ -29,8 +32,31 @@ function Home() {
         Работа в Photoshop
       </Tag>
       <Rating rating={rating} setRating={setRating} isEditable={true} />
+      <ul>
+        {menu.map((m, index) => (
+          <li key={index}>{m._id.secondCategory}</li>
+        ))}
+      </ul>
     </>
   );
 }
 
 export default withLayout(Home);
+
+export const getStaticProps: GetStaticProps<HomeProps> = async () => {
+  const firstCategory = 0;
+  const { data: menu } = await axios.post<MenuItem[]>(
+    process.env.NEXT_PUBLIC_DOMAIN + "/api/top-page/find",
+    {
+      firstCategory,
+    }
+  );
+  return {
+    props: { menu, firstCategory },
+  };
+};
+
+interface HomeProps extends Record<string, unknown> {
+  menu: MenuItem[];
+  firstCategory: number;
+}
