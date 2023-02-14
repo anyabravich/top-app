c774c72527ecee447c727316543414365984abcd
Annie <anniehartmann97@gmail.com>
2023-02-14 19:13:31 +0300

    Fix deploy

--- pages/courses/[alias].tsx
+++ pages/courses/[alias].tsx
@@ -9,24 +9,24 @@ function Course({ menu }: CourseProps) {
 
 export default withLayout(Course);
 
-export const getStaticProps: GetStaticProps<CourseProps> = async () => {
-  const firstCategory = 0;
-  const { data: menu } = await axios.post<MenuItem[]>(
-    process.env.NEXT_PUBLIC_DOMAIN + "/api/top-page/find",
-    {
-      firstCategory,
-    }
-  );
-  const { data: page } = await axios.post<MenuItem[]>(
-    process.env.NEXT_PUBLIC_DOMAIN + "/api/top-page/find",
-    {
-      firstCategory,
-    }
-  );
-  return {
-    props: { menu, firstCategory },
-  };
-};
+// export const getStaticProps: GetStaticProps<CourseProps> = async () => {
+//   const firstCategory = 0;
+//   const { data: menu } = await axios.post<MenuItem[]>(
+//     process.env.NEXT_PUBLIC_DOMAIN + "/api/top-page/find",
+//     {
+//       firstCategory,
+//     }
+//   );
+//   const { data: page } = await axios.post<MenuItem[]>(
+//     process.env.NEXT_PUBLIC_DOMAIN + "/api/top-page/find",
+//     {
+//       firstCategory,
+//     }
+//   );
+//   return {
+//     props: { menu, firstCategory },
+//   };
+// };
 
 interface CourseProps extends Record<string, unknown> {
   menu: MenuItem[];
