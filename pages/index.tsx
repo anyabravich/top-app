f32c3c3417f44a7509a3ca5a40f3d3185ab4acfb
Annie <anniehartmann97@gmail.com>
2023-02-14 19:24:33 +0300

    Test

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,7 @@ import { withLayout } from "../layout/Layout";
 import axios from "axios";
 import { MenuItem } from "../interfaces/menu.interface";
 
-function Home() {
+function Home({ menu }: HomeProps) {
   const [rating, setRating] = useState<number>(4);
   return (
     <>
@@ -43,18 +43,18 @@ function Home() {
 
 export default withLayout(Home);
 
-// export const getStaticProps: GetStaticProps<HomeProps> = async () => {
-//   const firstCategory = 0;
-//   const { data: menu } = await axios.post<MenuItem[]>(
-//     process.env.NEXT_PUBLIC_DOMAIN + "/api/top-page/find",
-//     {
-//       firstCategory,
-//     }
-//   );
-//   return {
-//     props: { menu, firstCategory },
-//   };
-// };
+export const getStaticProps: GetStaticProps<HomeProps> = async () => {
+  const firstCategory = 0;
+  const { data: menu } = await axios.post<MenuItem[]>(
+    process.env.NEXT_PUBLIC_DOMAIN + "/api/top-page/find",
+    {
+      firstCategory,
+    }
+  );
+  return {
+    props: { menu, firstCategory },
+  };
+};
 
 interface HomeProps extends Record<string, unknown> {
   menu: MenuItem[];
