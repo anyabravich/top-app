3da39387b93178926a1dbb6ec81378c9247a3db1
Annie <anniehartmann97@gmail.com>
2023-02-15 18:30:42 +0300

    Add search

--- layout/Sidebar/Sidebar.tsx
+++ layout/Sidebar/Sidebar.tsx
@@ -1,10 +1,10 @@
 import { SidebarProps } from "./Sidebar.props";
-import styles from "./Sidebar.module.scss";
-import cn from "classnames";
 import { Menu } from "../Menu/Menu";
+import { Search } from "../../components/Search/Search";
 
 export const Sidebar = ({ ...props }: SidebarProps) => (
   <div {...props}>
+    <Search />
     <Menu />
   </div>
 );
