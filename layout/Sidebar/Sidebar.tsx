42268021f168a032ef89ca8a8fabe272089a88ab
Annie <anniehartmann97@gmail.com>
2023-02-16 10:46:36 +0300

    Layout sidebar

--- layout/Sidebar/Sidebar.tsx
+++ layout/Sidebar/Sidebar.tsx
@@ -1,9 +1,13 @@
 import { SidebarProps } from "./Sidebar.props";
 import { Menu } from "../Menu/Menu";
 import { Search } from "../../components/Search/Search";
+import styles from "./Sidebar.module.scss";
+import Logo from "../logo.svg";
+import cn from "classnames";
 
-export const Sidebar = ({ ...props }: SidebarProps) => (
-  <div {...props}>
+export const Sidebar = ({ className, ...props }: SidebarProps) => (
+  <div className={cn(className, styles.sidebar)} {...props}>
+    <Logo className={styles.logo} />
     <Search />
     <Menu />
   </div>
