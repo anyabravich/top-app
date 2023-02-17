4db6966e7d1f032fcf38d6a4015f648a1a9c43f7
Annie <anniehartmann97@gmail.com>
2023-02-17 18:01:26 +0300

    Add animation for btn

--- components/Button/Button.props.ts
+++ components/Button/Button.props.ts
@@ -1,9 +1,12 @@
 import { ButtonHTMLAttributes, DetailedHTMLProps, ReactNode } from "react";
 
-export interface ButtonPage
-  extends DetailedHTMLProps<
-    ButtonHTMLAttributes<HTMLButtonElement>,
-    HTMLButtonElement
+export interface ButtonProps
+  extends Omit<
+    DetailedHTMLProps<
+      ButtonHTMLAttributes<HTMLButtonElement>,
+      HTMLButtonElement
+    >,
+    "onAnimationStart" | "onDragStart" | "onDragEnd" | "onDrag" | "ref"
   > {
   children: ReactNode;
   appearance: "primary" | "ghost";
