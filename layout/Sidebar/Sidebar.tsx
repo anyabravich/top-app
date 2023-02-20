af482b49adfb49e3fd1243480a4632be69a5b136
Annie <anniehartmann97@gmail.com>
2023-02-20 11:50:54 +0300

    Accessibility

--- layout/Sidebar/Sidebar.tsx
+++ layout/Sidebar/Sidebar.tsx
@@ -8,7 +8,7 @@ import Link from "next/link";
 
 export const Sidebar = ({ className, ...props }: SidebarProps) => (
   <div className={cn(className, styles.sidebar)} {...props}>
-    <Link href={"/"}>
+    <Link href={"/"} aria-label="логотип">
       <Logo className={styles.logo} />
     </Link>
     <Search />
